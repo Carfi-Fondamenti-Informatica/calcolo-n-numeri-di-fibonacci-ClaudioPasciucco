7d574a6cbf996757ea807fa9f773f00488e98056
ClaudioPasciucco <91784159+ClaudioPasciucco@users.noreply.github.com>
2021-11-11 12:35:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int i, n=0, x1=1, x2=1, x3=x1+x2;
+   cin >> n;
+   if (n>=2) {
+      cout << x1 << endl;
+      cout << x2 << endl;
+      for (i=2; i<n; i++){
+         x1 = x2;
+         x2 = x3;
+         x3 = x1+x2;
+         cout << x3 << endl;
+      }
+   }else{
+      cout << "errore" << endl;
+   }
    return 0;
 }
